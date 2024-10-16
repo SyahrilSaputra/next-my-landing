59109abeddb3351eb7dd19977a32ecbf12028fe9
SyahrilSaputra <syahrilsaputra2323@gmail.com>
2024-10-16 15:52:55 +0800

    main:

--- src/components/landing_layout_items/header.tsx
+++ src/components/landing_layout_items/header.tsx
@@ -13,7 +13,7 @@ export const Header = () => {
             </Link>
             <nav className="ml-auto flex gap-4 sm:gap-6">
                 <Link className="text-sm font-medium hover:underline underline-offset-4" href="#">
-                    Home
+                    Beranda
                 </Link>
                 <Link className="text-sm font-medium hover:underline underline-offset-4" href="#">
                     About Me
